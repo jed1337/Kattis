f15db5087a44dbd4d67bb6366b71d28e274b2c13
jed1337 <j.caychingco@yahoo.com>
2021-01-28 19:25:23 +0800

    Add solution for Free Weights

--- free_weights.py
+++ free_weights.py
@@ -5,28 +5,28 @@ def solve(row1, row2, all_weights):
     lo = 0
     hi = len(all_weights)
 
-    #O(log N)
     while lo < hi:
         mid = lo + (hi - lo) // 2
 
-        #O(N)
         filtered_row1 = keep_heavier_than(row1, all_weights[mid])
         filtered_row2 = keep_heavier_than(row2, all_weights[mid])
 
-        #O(N)
         if is_paired(filtered_row1) and is_paired(filtered_row2):
             hi = mid
         else:
             lo = mid + 1
+            row1 = filtered_row1
+            row2 = filtered_row2
 
     return all_weights[lo]
 
 
 def is_paired(row):
-    if len(row) % 2 == 1:
+    length = len(row)
+    if length % 2 == 1:
         return False
 
-    for i in range(0, len(row), 2):
+    for i in range(0, length, 2):
         if row[i] != row[i + 1]:
             return False
 
@@ -37,13 +37,9 @@ def keep_heavier_than(row, threshold):
     return list(filter(lambda x: x > threshold, row))
 
 
-# input()
-# row1 = list(map(int, input().split()))
-# row2 = list(map(int, input().split()))
-
-row1=[7,7,15,4,4,5,5,3,3,9,9,1,1]
-row2=[2,2, 15]
-
+input()
+row1 = list(map(int, input().split()))
+row2 = list(map(int, input().split()))
 
 all_weights = sorted(list(set(row1 + row2)))
 
