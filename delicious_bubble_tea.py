8816c2e6b6fca68c35b4c4a7283054acca4867df
jed1337 <j.caychingco@yahoo.com>
2021-02-26 20:39:59 +0800

    Add solution for Delicious Bubble Tea

--- delicious_bubble_tea.py
+++ delicious_bubble_tea.py
@@ -1,18 +1,30 @@
 from math import floor
 
+
+def get_min_topping_cost(combinations, topping_dict):
+    min_topping_cost = int(1e10)
+    for combination in combinations:
+        min_topping_cost = min(min_topping_cost, topping_dict[combination])
+
+    return min_topping_cost
+
+
 tea_count = int(input())
 tea_prices = list(map(int, input().split()))
-int(input())
+topping_count = int(input())
 topping_prices = list(map(int, input().split()))
 
-for i in range(tea_count):
-    combination_prices = list(map(int, input().split()))
-    combination_prices = combination_prices[1:]
+topping_dict = {}
+for i in range(topping_count):
+    topping_dict[i] = topping_prices[i]
 
-    tea_prices[i] += min(combination_prices)
+for i in range(tea_count):
+    combinations = list(map(lambda x: int(x) - 1, input().split()))
+    combinations = combinations[1:]
+
+    tea_prices[i] += get_min_topping_cost(combinations, topping_dict)
 
 money = int(input())
-
 cheapest = sorted(tea_prices)[0]
 max_students = floor(money / cheapest) - 1
 
